b3230175d79a2508631b40b90c60dc38d8d9cf05
Márcio J. Silva <105254896+ImMarcio@users.noreply.github.com>
2023-04-13 17:34:29 -0300

    Initial Commit

---
+++ IHCPROJETO/scripts/heartPageScript.js
@@ -0,0 +1,38 @@
+// Variáveis
+const listaFavoritos = document.getElementById('lista-favoritos');
+const btnAdicionar = document.getElementById('btn-adicionar');
+
+// Eventos
+btnAdicionar.addEventListener('click', adicionarFavorito);
+
+// Funções
+function adicionarFavorito() {
+  const url = prompt('Digite a URL do site:');
+  const nome = prompt('Digite o nome do site:');
+  const imagem = prompt('Digite a URL da imagem do site:');
+  
+  if (url && nome && imagem) {
+    const li = document.createElement('li');
+    const img = document.createElement('img');
+    const a = document.createElement('a');
+    const span = document.createElement('span');
+    
+    img.src = imagem;
+    a.href = url;
+    a.textContent = nome;
+    span.textContent = 'x';
+    span.classList.add('remover-favorito');
+    
+    li.appendChild(img);
+    li.appendChild(a);
+    li.appendChild(span);
+    listaFavoritos.appendChild(li);
+    
+    span.addEventListener('click', removerFavorito);
+  }
+}
+
+function removerFavorito() {
+  const li = this.parentElement;
+  listaFavoritos.removeChild(li);
+}
---
+++ IHCPROJETO/scripts/script.js
@@ -0,0 +1,6 @@
+var searchBtn = document.getElementById("search-btn");
+var searchForm = document.getElementById("search-form");
+
+searchBtn.addEventListener("click", function() {
+  searchForm.classList.toggle("d-none");
+});

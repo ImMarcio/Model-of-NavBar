5e2f3567490bccea65d474c925a4950c504a7a4c
Márcio J. Silva <105254896+ImMarcio@users.noreply.github.com>
2023-04-27 12:52:59 -0300

    Update heartPageScript.js

--- IHCPROJETO/scripts/heartPageScript.js
+++ IHCPROJETO/scripts/heartPageScript.js
@@ -36,3 +36,11 @@ function removerFavorito() {
   const li = this.parentElement;
   listaFavoritos.removeChild(li);
 }
+
+
+var searchBtn = document.getElementById("search-btn");
+var searchForm = document.getElementById("search-form");
+
+searchBtn.addEventListener("click", function() {
+  searchForm.classList.toggle("d-none");
+});
